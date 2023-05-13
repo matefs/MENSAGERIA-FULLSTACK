c878700b4ac9c68ef5e72796b0f2f65cbe2a0483
Mateus Schverz <mate_fs@hotmail.com>
2023-05-13 17:18:55 -0300

    add: mensagem é enviada e recebida como objeto pelo cliente

--- cliente-frontend-vite/src/App.jsx
+++ cliente-frontend-vite/src/App.jsx
@@ -4,20 +4,32 @@ import io from "socket.io-client";
 function SocketIODemo() {
   const socket = io("http://localhost:3000");
 
+  const notification = {
+  id: 1,
+  usuarioNotificacao: "Mateus",
+  dataCriacao: new Date(),
+  conteudo: {
+    titulo: "Titulo da notificacao",
+    descricao: "Esta notificacao foi enviada de um cliente para todos outros",
+  },
+  origem: "Cliente web React",
+  estado: "Não lida",
+  categoria: "Informativo"
+  };
+  
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Connected to server");
     });
     
   }, []);
-  
 
   socket.on("messageBroadcast", (message) => {
-  alert(message)
+  alert(message.conteudo.descricao)
   });
 
   const handleSubmit = () => {
-    socket.emit("messageBroadcast", "Mensagem enviada pelo primeiro cliente");
+    socket.emit("messageBroadcast", notification);
   };
 
   return (
