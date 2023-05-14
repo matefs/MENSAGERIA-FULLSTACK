dd53fbd5b6bf9fdec6a42f3f3fd3917b76d9d025
Mateus Schverz <mate_fs@hotmail.com>
2023-05-14 01:11:26 -0300

    fix: removido comentarios desnecessário  e ajustado identação geral

--- cliente-frontend-vite/src/App.jsx
+++ cliente-frontend-vite/src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
-import { Alert } from 'antd';
-
+import { Alert } from "antd";
 
 function SocketIODemo() {
-//  var socket = io("http://localhost:3000");
-  var socket = useRef(null) 
+  var socket = useRef(null);
 
   const notification = {
-  usuarioNotificacao: "Mateus",
-  dataCriacao: new Date(),
-  conteudo: {
-    titulo: "Titulo da notificacao",
-    descricao: "Esta notificacao foi enviada de um cliente para todos outros",
-  },
-  origem: "Cliente web React",
-  foiLida: false,
-  categoria: "info"
+    usuarioNotificacao: "Mateus",
+    dataCriacao: new Date(),
+    conteudo: {
+      titulo: "Titulo da notificacao",
+      descricao: "Esta notificacao foi enviada de um cliente para todos outros",
+    },
+    origem: "Cliente web React",
+    foiLida: false,
+    categoria: "info",
   };
-  
 
-  const [existeMensagem, setExisteMensagem ] = useState({
+  const [existeMensagem, setExisteMensagem] = useState({
     usuarioNotificacao: "",
     dataCriacao: "",
     conteudo: {
@@ -29,66 +26,52 @@ function SocketIODemo() {
     },
     origem: "",
     foiLida: false,
-    categoria: "", 
-    ativa: false
-  })
-
-  useEffect(() => { 
-  socket.current = io("http://localhost:3000");
-  
-  socket.current.on("connect", () => {
-    console.log("Connected to server");
+    categoria: "",
+    ativa: false,
   });
 
-  
-  socket.current.on("messageBroadcast", (message) => {
-  //alert(message.conteudo.descricao)
-  setExisteMensagem({ 
-    ...message,
-    ativa: true
-  })
+  useEffect(() => {
+    socket.current = io("http://localhost:3000");
 
-  }); 
+    socket.current.on("connect", () => {
+      console.log("Connected to server");
+    });
 
- 
+    socket.current.on("messageBroadcast", (message) => {
+      setExisteMensagem({
+        ...message,
+        ativa: true,
+      });
+    });
   }, []);
 
-/*   socket.on("messageBroadcast", (message) => {
-  //alert(message.conteudo.descricao)
-  setExisteMensagem({ 
-    ...message,
-    ativa: true
-  })
-
-  });  */
-
-  const handleSubmit = () => { 
-    socket.current.emit("messageBroadcast", notification); 
+  const handleSubmit = () => {
+    socket.current.emit("messageBroadcast", notification);
   };
 
   return (
     <div>
+      <div id="centralizador">
+        <button onClick={handleSubmit}>Notificar outros usuarios</button>
+      </div>
 
-<div id="centralizador">
-      <button onClick={handleSubmit}>Notificar outros usuarios</button>
-</div>
-
-  <div style={{ width: '50%', position: 'fixed', bottom: 10, left: 0 }}> 
-
- { existeMensagem.ativa == true && <Alert
-      message={existeMensagem.conteudo.titulo}
-      description={existeMensagem.conteudo.descricao}
-      type={existeMensagem.categoria}
-      showIcon 
-      onClick={() => setExisteMensagem({
-      ...existeMensagem,
-      ativa: false
-    }) }
-    style={{cursor:'pointer'}}
-
-    /> }
-  </div>
-
+      <div style={{ width: "50%", position: "fixed", bottom: 10, left: 0 }}>
+        {existeMensagem.ativa == true && (
+          <Alert
+            message={existeMensagem.conteudo.titulo}
+            description={existeMensagem.conteudo.descricao}
+            type={existeMensagem.categoria}
+            showIcon
+            onClick={() =>
+              setExisteMensagem({
+                ...existeMensagem,
+                ativa: false,
+              })
+            }
+            style={{ cursor: "pointer" }}
+          />
+        )}
+      </div>
     </div>
   );
 }
